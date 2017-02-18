9d71e5c5200d79f5133484b445bdc35cd1dc6cdc
Chethanadesicrew <chethanakotian@desicrew.in>
2017-02-18 13:45:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,65 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone - {
+title: 'Article One / chethana',
+heading: 'Article One',
+date: 'February 16th, 2017',
+content:     
+                <p>
+                    Spear fisherman Danny Henricks is fortunate enough to have survived the type of underwater experience that proves just how dangerous sea creatures can be -- and he even caught it all on video.
+                    Henricks, 35, was fishing off the coast of Australia when a bull shark charged at him at full speed, only to impale himself on Danny's spear gun.
+                    The hair-raising clip has been video more than three million times (and counting) since Henricks uploaded it last month.
+                </p> '
+
+
+
+};
+
+function createtemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+var htmltemplate = 
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+        <body>
+            <div class="container">
+            <div>
+                <a href='/'>Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+               ${content}
+                </div>
+            </div>
+        </body>
+    </head>
+</html>
+   ;
+   return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleone));
 });
 app.get('/article-two', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
