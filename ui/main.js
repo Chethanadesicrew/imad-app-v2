f61c87962e0694643aaecf687bae2e80d7c1e61e
Chethanadesicrew <chethanakotian@desicrew.in>
2017-02-23 16:14:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,15 @@
-console.log('Loaded!');
+//Counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-//change the text of the main-text div
-var element = document.getElementById('main-text');
+button.oneclick = function () {
 
-element.innerHTML = 'New value';
-
-// Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 'px';
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.oneclick = function () {
-   var interval = setInterval(moveRight, 50);
+  // Make a request to the counter endpoint
+  
+  //Capture the response and store it in a variable
+  
+  //Render the variable in the correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
