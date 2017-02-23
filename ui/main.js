0a7f7137632a557a08b3e4fdebf56492c01c1573
Chethanadesicrew <chethanakotian@desicrew.in>
2017-02-23 16:43:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,26 @@
 //Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.oneclick = function () {
 
-  // Make a request to the counter endpoint
+  // Create a request object
+  var request = new XMLHttpRequest();
   
-  //Capture the response and store it in a variable
   
-  //Render the variable in the correct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+  if(request.readyState === XMLHttpRequest.DONE)  {
+      // Take some action
+      if (request status === 200) {
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+      }
+  }  
+    // Not done yet  
+  };
+  
+  // Make the request
+  request.open('GET', 'http://chethanadesicrew.imad.hasura-app.io/counter', true);
+  req.send(null);
 };
