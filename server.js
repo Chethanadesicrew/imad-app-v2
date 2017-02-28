b83fa54d41c3f87a92590598640dbdf21d2ae873
Chethanadesicrew <chethanakotian@desicrew.in>
2017-02-28 16:42:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/submit-name/:name', function (req, res) {
    names.push(name);
    // JSON: Javascript Object Notation
    
-   res.send(JSON.stringifynames));
+   res.send(JSON.stringify(names));
 });
 
 
