b8390b7b3b3fc4f6f6bc504a7a4c11aed067d649
Chethanadesicrew <chethanakotian@desicrew.in>
2017-02-28 17:08:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,17 @@ request.onreadystatechange = function () {
     if (request.readyState === XMLHttpRequest.DONE) {
         // Take some action
         if (request.status ===200) {
-            var counter = reqest.resposeText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
+            
+             // Capture a list of names and render it as a list.
+ var names = request.responseText;
+ names = JSON.parse(names);
+ var list = '';
+ for (var i=0; i< names.length; i++) {
+     list += '<li>' + names[i] + '</li>';
+ }
+ var ul = document.getElementById('namelist');
+ ul.innerHTML = list;
+
 
         }
     }
@@ -23,7 +31,7 @@ request.onreadystatechange = function () {
 };
 
 // Make the request
-request.open('GET', 'http://chethanadesicrew.imad.hasura-app.io/counter', true);
+request.open('GET', 'http://chethanadesicrew.imad.hasura-app.io/submit-name?name=' + name, true);
 request.send(null);
 
 };
@@ -35,12 +43,4 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
  // Make a request to the server and send the name
  
- // Capture a list of names and render it as a list.
- var names = ['name1', 'name2', 'name3', 'name4'];
- var list = '';
- for (var i=0; i< names.length; i++) {
-     list += '<li>' + names[i] + '</li>';
- }
- var ul = document.getElementById('namelist');
- ul.innerHTML = list;
 };
