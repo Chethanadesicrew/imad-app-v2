165473b51cdd198ff961cbdbcd1d78442a409f3a
Chethanadesicrew <chethanakotian@desicrew.in>
2017-03-10 10:40:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
+var Pool = require('pg').Pool;
 
 var config = {
-    user: 'chethanadesicrew',
-    database: 'chethanadesicrew',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
+  //host: 'db.imad.hasura-app.io',
+  //user: 'chethanadesicrew',
+  //database: 'chethanadesicrew',
+  //port: '5432',
+  //password: process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
@@ -86,14 +86,16 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     // Make a select request
-    // return a response with a results
+    // return a response with the results
     pool.query('SELECT * FROM test', function (err, result) {
         if (err) {
-            res.status(500).send(err.toString());
+           res.status(500).send(err.toString()); 
         } else {
             res.send(JSON.stringify(result));
         }
+        
     });
+    
     
 });
 
