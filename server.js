6f1c5444e48364861a9ffefbb4ad4ee4bce1a682
Chethanadesicrew <chethanakotian@desicrew.in>
2017-03-13 16:21:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
 
-var config = {
-  host: 'db.imad.hasura-app.io',
-  user: 'chethanadesicrew',
-  database: 'chethanadesicrew',
-  port: '5432',
-  password: db-chethanadesicrew-71786
-};
 var app = express();
 app.use(morgan('combined'));
 
 
+var articles = {
+'article-one':  {
+    title: 'Article one Chethana',
+    heading:'Article one',
+    date: 'Feb 16, 2017',
+    content: ` <p> 
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p> 
+             <p> 
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+             <p> 
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>`
+},
+'article-two': {
+    title: 'Article Two Chethana',
+    heading:'Article Two',
+    date: 'Feb 16, 2017',
+    content: ` <p> 
+            This is the content for my second article.
+            </p>`
+},
+'article-three': {
+    title: 'Article Three Chethana',
+    heading:'Article Three',
+    date: 'Feb 16, 2017',
+    content: ` <p> 
+            This is the content for my third article.
+            </p>`
+}
+};
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
@@ -38,7 +62,7 @@ var htmlTemplate =`
             ${heading}
         </h3>
         <div>
-            ${date.toDateString()}
+            ${date}
         </div>
         <div>
 ${content}
@@ -57,56 +81,30 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
-app.get('/test-db', function (req, res) {
-    // Make a select request
-    // return a response with the results
-    pool.query('SELECT * FROM test', function (err, result) {
-        if (err) {
-           res.status(500).send(err.toString()); 
-        } else {
-            res.send(JSON.stringify(result.rows));
-        }
-        
-    });
-    
-    
-});
-
 var counter = 0;
 app.get('/counter', function (req, res) {
-    counter = counter +1;
+    counter = counter + 1;
     res.send(counter.toString());
+    
 });
 
 var names = [];
-app.get('/submit-name', function (req, res) { // URL: /submit-name?name=XXXXX
-   // Get the name from the request
-   var name = req.query.name;
+app.get('/submit-name', function(req, res) { // URL: /submit-name?name=xxxx
+    // Get the name from the request 
+    var name = req.query.name;
    
-   names.push(name);
-   // JSON: Javascript Object Notation
-   
-   res.send(JSON.stringify(names));
+       names.push(name);
+        // JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
+    
 });
 
-app.get('/articles/:articleName', function (req, res) {
+
+app.get('/:articleName', function (req, res) {
     // articleName == article-one
     // articles[articleName] == {} content object for article one
-    
-    // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-    pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName], function(err, result) {
-       if (err) {
-           res.status(500).send(err.toString());
-       } else {
-           if (result.rows.length === 0) {
-               res.status(404).send('Article not found');
-           } else {
-               var articleData = result.rows[0];
-                res.send(createTemplate(articleData));    
-           }
-       }
-    });
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));    
 });
 
 app.get('/ui/style.css', function (req, res) {
