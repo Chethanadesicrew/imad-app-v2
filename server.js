3f54447b8456e605b159e89c97c0e1bf46ec1828
Chethanadesicrew <chethanakotian@desicrew.in>
2017-03-14 14:50:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'chethanadesicrew',
@@ -75,6 +76,10 @@ app.get('/hash/:input', function(req, res) {
 
 app.post('/create user', function (req, res) {
     // username, password
+    // JSON 
+    var username = req.body.username;
+    var password = req.body.password;
+    
     var salt = crypto.getRandomBytes(128).toString('hex');
     var doString = hash(password, salt);
     pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, doString], function (err, result) {
