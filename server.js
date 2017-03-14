a03a02b5e3fde54078a9ef3fc8bcf870d3ab9175
Chethanadesicrew <chethanakotian@desicrew.in>
2017-03-14 11:35:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/articles/:articleName', function (req, res) {
     // articles[articleName] == {} content object for article one
  
 
-// SELECT * FROM article WHERE ttitle = 'article-one'
+// SELECT * FROM article WHERE ttitle = ''; DELETE WHERE a = 'asdf'
 pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
     if (err) {
         res.status(500).send(err.toString());
