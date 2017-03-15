01e04975fd4064e754e1d75cbfc0a2bcfaeb8368
Chethanadesicrew <chethanakotian@desicrew.in>
2017-03-15 11:42:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,4 +30,5 @@ console.log(password);
 request.open('POST', 'http://chethanadesicrew.imad.hasura-app.io/login', true);
 request.setRequestHeader('Content-type', 'application/json');
 request.send(JSON.stringify({username: username, password: password}));
+
 };
